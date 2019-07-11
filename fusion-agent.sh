0a2c6beaa259875515aee0cdcbe9be4e89ed329f
Jonathanyyf <jonathan@jyyf.work>
2019-07-12 00:30:52 +0200

    input SERVER variable

--- fusion-agent.sh
+++ fusion-agent.sh
@@ -3,7 +3,8 @@
 # Official documentation for Debian/Ubuntu
 # http://fusioninventory.org/documentation/agent/installation/linux/deb.html
 #
-SERVER="path of you fusioninventory server"
+
+read -p "Enter path of your plugin (ex: https://server.domain.com/glpi/plugins/fusioninventory/): " SERVER
 
 apt -y install dmidecode hwdata ucf hdparm perl libuniversal-require-perl libwww-perl libparse-edid-perl libproc-daemon-perl libfile-which-perl libhttp-daemon-perl libxml-treepp-perl libyaml-perl libnet-cups-perl libnet-ip-perl libdiges$wget https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.1/fusioninventory-agent_2.5.1-1_all.deb -P /tmp/
 dpkg -i /tmp/fusioninventory-agent_2.5.1-1_all.deb
