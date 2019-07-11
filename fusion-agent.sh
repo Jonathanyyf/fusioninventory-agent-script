6ccf20e92ce765ea4aa6265025572086aa714f55
Jonathan Yang You Fu <jonathan@jyyf.work>
2019-07-11 23:11:37 +0200

    1st commit
    Base script

---
+++ fusion-agent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Official documentation for Debian/Ubuntu
+# http://fusioninventory.org/documentation/agent/installation/linux/deb.html
+#
+SERVER="path of you fusioninventory server"
+
+apt -y install dmidecode hwdata ucf hdparm perl libuniversal-require-perl libwww-perl libparse-edid-perl libproc-daemon-perl libfile-which-perl libhttp-daemon-perl libxml-treepp-perl libyaml-perl libnet-cups-perl libnet-ip-perl libdiges$wget https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.1/fusioninventory-agent_2.5.1-1_all.deb -P /tmp/
+dpkg -i /tmp/fusioninventory-agent_2.5.1-1_all.deb
+sed -i "/#server = http:\/\/server.domain.com\/glpi\/plugins\/fusioninventory\//aserver = http:\/\/$SERVER\/glpi\/plugins\/fusioninventory/" /etc/fusioninventory/agent.cfg
+systemctl restart fusioninventory-agent
+fusioninventory-agent
