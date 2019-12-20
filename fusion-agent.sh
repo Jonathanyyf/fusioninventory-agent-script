d8982b8139bd2596f854b1c8471f5da9fdc34f7c
Jonathan Yang You Fu <jonathan@jyyf.work>
2019-12-20 12:05:55 +0100

    Update fusion-agent.sh
    error break line

--- fusion-agent.sh
+++ fusion-agent.sh
@@ -6,7 +6,8 @@
 
 read -p "Enter path of your plugin (ex: https://server.domain.com/glpi/plugins/fusioninventory/): " SERVER
 
-apt -y install dmidecode hwdata ucf hdparm perl libuniversal-require-perl libwww-perl libparse-edid-perl libproc-daemon-perl libfile-which-perl libhttp-daemon-perl libxml-treepp-perl libyaml-perl libnet-cups-perl libnet-ip-perl libdiges$wget https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.1/fusioninventory-agent_2.5.1-1_all.deb -P /tmp/
+apt -y install dmidecode hwdata ucf hdparm perl libuniversal-require-perl libwww-perl libparse-edid-perl libproc-daemon-perl libfile-which-perl libhttp-daemon-perl libxml-treepp-perl libyaml-perl libnet-cups-perl libnet-ip-perl libdiges
+wget https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.1/fusioninventory-agent_2.5.1-1_all.deb -P /tmp/
 dpkg -i /tmp/fusioninventory-agent_2.5.1-1_all.deb
 sed -i "/#server = http:\/\/server.domain.com\/glpi\/plugins\/fusioninventory\//aserver = http:\/\/$SERVER\/glpi\/plugins\/fusioninventory/" /etc/fusioninventory/agent.cfg
 systemctl restart fusioninventory-agent
