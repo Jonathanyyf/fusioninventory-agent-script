94c5f11d0421c2118336f2fe100c5b52e0f7391a
Jonathan Yang You Fu <jonathan@jyyf.work>
2020-08-16 02:04:53 +0200

    Precise instructions
    ask for the domain name and not the full path

--- fusion-agent.sh
+++ fusion-agent.sh
@@ -4,7 +4,7 @@
 # http://fusioninventory.org/documentation/agent/installation/linux/deb.html
 #
 
-read -p "Enter path of your plugin (ex: https://server.domain.com/glpi/plugins/fusioninventory/): " SERVER
+read -p "Enter the domain of your glpi (ex: server.domain.com): " SERVER
 
 apt -y install dmidecode hwdata ucf hdparm perl libuniversal-require-perl libwww-perl libparse-edid-perl libproc-daemon-perl libfile-which-perl libhttp-daemon-perl libxml-treepp-perl libyaml-perl libnet-cups-perl libnet-ip-perl libdiges
 wget https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.1/fusioninventory-agent_2.5.1-1_all.deb -P /tmp/
